48210262cba9ca079b32547d7afbb21e5a21f0ad
Андрей Агеев <ageevweb@PONI.local>
2020-01-13 13:31:14 +0300

    pug + header

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ let express = require('express');
 let app = express();
 app.use(express.static('public'));
 
+app.set('view engine', 'pug');
 
 app.listen(3000, function(){
   console.log('node express works on 3000');
 });
 
 app.get('/', function(req, res){
-  console.log('/ loaded')
-  res.render('index.html');
+  // console.log('/ loaded');
+  res.render('main', {
+    title: 'titleFIRST'
+  });
 });
+
+
 
 // app.get('/cat', function(req, res){
 //   res.end('hello22');
