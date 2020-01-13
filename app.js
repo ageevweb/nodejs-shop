79c0386a50fad45b9b8408108a3dbde187a710b0
Андрей Агеев <ageevweb@PONI.local>
2020-01-13 16:08:43 +0300

    all items on page

--- app.js
+++ app.js
@@ -4,22 +4,40 @@ app.use(express.static('public'));
 
 app.set('view engine', 'pug');
 
+// add mysql modul : npm install mysql
+let mysql = require('mysql');
+let con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'datalist'
+});
+
+
 app.listen(3000, function(){
   console.log('node express works on 3000');
 });
 
+
 app.get('/', function(req, res){
-  // console.log('/ loaded');
-  res.render('main', {
-    title: 'titleFIRST'
-  });
+  con.query('SELECT * FROM goods', function(error, result){
+      if (error) throw err;
+      // console.log(result);
+      
+      // repack obj important
+      let goods = {};
+      for(let i=0; i<result.length; i++){
+        goods[result[i]['id']] = result[i];
+      }
+      // console.log(goods[5]['name'])
+      res.render('main', {
+        title: 'titleFIRST',
+        goods: JSON.parse(JSON.stringify(goods))
+      });
+      // console.log(JSON.parse(JSON.stringify(goods)));
+    }
+  );
+  
 });
-
-
-
-// app.get('/cat', function(req, res){
-//   res.end('hello22');
-// });
-
 
 
