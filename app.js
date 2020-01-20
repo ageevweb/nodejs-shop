038e9b5d2e7597d60297033c04299079c950999d
Андрей Агеев <ageevweb@PONI.local>
2020-01-20 16:18:52 +0300

    concat tables /admin-order

--- app.js
+++ app.js
@@ -251,7 +251,23 @@ app.get('/admin', function(req, res){
 
 
 app.get('/admin-order', function(req, res){
-  con.query('SELECT *,  from_unixtime(date, "%Y-%m-%d %h:%m")as human_date FROM shop_order ORDER BY id DESC', 
+  con.query(`
+    SELECT 
+      shop_order.id as id,
+      shop_order.user_id as user_id,
+      shop_order.goods_id as goods_id,
+      shop_order.goods_cost as goods_cost,
+      shop_order.goods_amount as goods_amount,
+      shop_order.total as total,
+      from_unixtime(date,"%Y-%m-%d %h:%m") as human_date,
+      user_info.user_name as user,
+      user_info.user_phone as phone,
+      user_info.address as address
+    FROM 
+      shop_order
+    LEFT JOIN	
+      user_info
+    ON shop_order.user_id = user_info.id ORDER BY id DESC`,
   function(error, result, fields){
     if (error) reject(error);
     res.render('admin-order', {
